2d19eb039dc3e735cd5c9cadf78d80b6432e26da
andrewmyhre <andrew@justgiving.com>
2013-11-04 10:55:01 -0500

    routes ignore index.html

--- src/Tech-Rewards/App_Start/RouteConfig.cs
+++ src/Tech-Rewards/App_Start/RouteConfig.cs
@@ -12,6 +12,7 @@ namespace Tech_Rewards
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("index.html");
 
             routes.MapRoute(
                 name: "Default",

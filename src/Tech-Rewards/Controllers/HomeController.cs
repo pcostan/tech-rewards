2f4f9087876c9c66f4dc706186398994c42979df
andrewmyhre <andrew@justgiving.com>
2013-11-04 11:10:59 -0500

    fixes default page and static file serving

---
+++ src/Tech-Rewards/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Mvc;
+
+namespace Tech_Rewards.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [OutputCache(Duration = (60*24))]
+        public ActionResult Index()
+        {
+            return new FilePathResult("~/index.html", "text/html");
+        }
+	}
+}
